6dad0f68af3086d1f071d48c87cb366310367243
Panos Stavrianos <pstavrianosmail@gmail.com>
2021-07-02 16:39:17 +0300

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+# syntax=docker/dockerfile:experimental
+FROM python:3.8-slim
+
+WORKDIR /usr/data
+RUN mkdir -p /usr/app-data
+
+COPY requirements.txt /usr/src/app/requirements.txt
+WORKDIR /usr/src/app
+RUN --mount=type=cache,target=/root/.cache/pip pip install -r requirements.txt
+
+COPY ./ /usr/src/app
+
+EXPOSE 5050
+
+CMD gunicorn server:app
+
+# DOCKER_BUILDKIT=1 docker build -t comet.app.orbitsystems.gr/maze_http_server:1.0.0 .
+# docker run -p "5050:5050" comet.app.orbitsystems.gr/maze_http_server:1.0.0
+# docker push comet.app.orbitsystems.gr/maze_http_server:1.0.0
---
+++ server.py
@@ -0,0 +1,101 @@
+from flask import Flask, request
+from queue import Queue
+from flask_cors import CORS
+from environs import Env
+
+env = Env()
+
+agent = Queue(maxsize=1)
+player = Queue(maxsize=1)
+app = Flask(__name__)
+CORS(app)
+config_data = None
+timeout = env('TIMEOUT', 5)
+
+
+def message_by(queue: Queue, data: dict):
+    if queue.full():
+        queue.get()
+        # print(f"replacing {removed} with {data}")
+    queue.put(data)
+
+
+@app.route("/config", methods=["POST", "GET"])
+def config():
+    global config_data
+    if request.method == 'GET':
+        return config_data
+    if request.method == 'POST':
+        config_data = request.json
+        return {"OK": 'OK'}
+
+
+@app.route("/")
+def home():
+    return "This is a http server for the maze experiment"
+
+
+@app.route("/agent_ready")
+def agent_ready():
+    """Initiate by agent"""
+    #      print('agent->agent_ready')
+    return assert_player_command(player.get(timeout=timeout), 'player_ready')
+
+
+@app.route("/player_ready")
+def player_ready():
+    """Initiate by player"""
+    #      print('player->player_ready')
+    message_by(player, {'command': 'player_ready'})
+    return agent.get(timeout=timeout)
+
+
+@app.route("/reset")
+def reset():
+    """Initiate by agent"""
+    #      print('agent->reset')
+    message_by(agent, {'command': 'reset'})
+    return assert_player_command(player.get(timeout=timeout), 'reset')
+
+
+@app.route("/reset_done", methods=["POST"])
+def reset_done():
+    """Initiate by player"""
+    #      print('player->reset_done')
+    message_by(player, request.json)
+    return agent.get(timeout=timeout)
+
+
+@app.route("/step", methods=["POST"])
+def step():
+    """Initiate by agent"""
+    # #      print('agent->step')
+    message_by(agent, {'command': 'step', "step_request": request.json})
+    return assert_player_command(player.get(timeout=timeout), 'step')
+
+
+@app.route("/observation", methods=["POST"])
+def observation():
+    """Initiate by player"""
+    # #      print('player->observation')
+    message_by(player, request.json)
+    return agent.get(timeout=timeout)
+
+
+@app.route("/training", methods=["POST"])
+def training():
+    """Initiate by agent"""
+    # print('agent->training', request.json)
+    message_by(agent, {'command': 'training', "training_request": request.json})
+    return {"OK": 'OK'}
+
+
+def assert_player_command(res, command):
+    if 'command' in res and res['command'] == command:
+        return res
+    # print("ERROR COMMAND", res, command)
+    return player.get(timeout=timeout)
+
+
+if __name__ == "__main__":
+    app.run(port=5050, host="0.0.0.0")
